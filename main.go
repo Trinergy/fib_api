7d96f8ed7468ac17167e6da4563d939b92d4ccfb
Kenny Wu <contact@kennywupaints.com>
2020-10-06 12:56:43 -0700

    Add DB lifecycle methods and interface

--- main.go
+++ main.go
@@ -11,25 +11,32 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// DB is a global variable for all handlers to access
-var DB *bolt.DB
-
 const (
 	dbName = "my.db"
 	bucket = "fibonnaciBucket"
 )
 
+// DataStore is a key value store which has two methods:
+// Get() - for fetching keys with value
+// Set() - for setting keys with value
+type DataStore interface {
+	Get(key string) (string, error)
+	Set(key string, value string) error
+}
+
+type dbStore struct {
+	bucket string
+	db     *bolt.DB
+}
+
+var store DataStore
+
 // Current returns the value of the current fibonacci sequence
 func Current(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	DB.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(bucket))
-		idx := b.Get([]byte("index"))
-		v := b.Get([]byte(idx))
+	idx, _ := store.Get("index")
+	value, _ := store.Get(idx)
 
-		message := fmt.Sprintf("%s", v)
-		io.WriteString(w, message)
-		return nil
-	})
+	io.WriteString(w, value)
 }
 
 // Next returns the value of the next number in the fibonacci sequence
@@ -44,16 +51,30 @@ func main() {
 	// Setup Routes
 	router := httprouter.New()
 	router.GET("/current/", Current)
-	router.PUT("/next", Next)
+	router.GET("/next", Next)
 	router.GET("/previous", Previous)
 
 	// Setup DB
+	db := newDB()
+	err := db.configure()
+	if err != nil {
+		log.Fatal(err)
+	}
+	store = db
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func newDB() *dbStore {
 	db, err := bolt.Open(dbName, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-	err = db.Update(func(tx *bolt.Tx) error {
+	return &dbStore{bucket: bucket, db: db}
+}
+
+func (store *dbStore) configure() error {
+	return store.db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
 		if err != nil {
 			return fmt.Errorf("create bucket: %s", err)
@@ -65,10 +86,34 @@ func main() {
 
 		return nil
 	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	DB = db
+}
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+func (store *dbStore) Get(key string) (string, error) {
+	var returnValue []byte
+
+	err := store.db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(store.bucket))
+		v := b.Get([]byte(key))
+
+		returnValue = make([]byte, len(v))
+		copy(returnValue, v)
+
+		fmt.Printf("GET CALL: %s", returnValue)
+
+		return nil
+	})
+
+	return fmt.Sprintf("%s", returnValue), err
+}
+
+func (store *dbStore) Set(key string, value string) error {
+	return store.db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(store.bucket))
+		err := b.Put([]byte(key), []byte(value))
+		if err != nil {
+			return fmt.Errorf("set key: %s", err)
+		}
+
+		return nil
+	})
 }
